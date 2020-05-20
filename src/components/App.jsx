f740672fe9ec1fa6138422199deb536746239804
AugustBemsen <54921134+AugustBemsen@users.noreply.github.com>
2020-05-20 02:15:29 +0100

    Adding Decrement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  let state = "";
   const increment = () => {
     setCount(count + 1);
   };
+  const decrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="container">
+      <p>{state}</p>
       <h1>{count}</h1>
       <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 };
